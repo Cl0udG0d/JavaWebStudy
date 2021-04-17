8abfd84c13dc248f4aa72ce41be87f8008e25812
潘一 <2585614464@qq.com>
2021-04-17 21:51:26 +0800

    add unit12 code

--- JavaWebTest/src/cn/web/courseEX12/ValidateServlet.java
+++ JavaWebTest/src/cn/web/courseEX12/ValidateServlet.java
@@ -1,4 +1,30 @@
 package cn.web.courseEX12;
 
-public class ValidateServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/ValidateServlet")
+public class ValidateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session=req.getSession();
+        String inputCode=req.getParameter("code");
+        String code= (String) session.getAttribute("randStr");
+        if(inputCode.equals(code)){
+            resp.sendRedirect("/Unit12/main.jsp");
+        }else{
+            resp.sendRedirect("/Unit12/login.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
 }
